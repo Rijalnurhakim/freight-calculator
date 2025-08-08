fb6b51998462cfccc9868f8e81b8ff59cd5b96e1
Rijal <rijal.nurhakim@ymail.com>
2025-08-08 20:59:33 +0700

    Init Pertama: setup django, virtualenv, shipping app, models

---
+++ shipping/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from shipping.models import Country, Category
+
+admin.site.register(Country)
+admin.site.register(Category)
---
+++ shipping/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Country(models.Model):
+    country_name = models.CharField(max_length=100)
+    country_flag = models.URLField()
+    country_currency = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.country_name
+
+class Category(models.Model):
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    category_title = models.CharField(max_length=100)
+    price_per_kilo = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.category_title
